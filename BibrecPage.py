a290d736f1f3fb784a6c8d5ffc9a6b98d2c446a9
eric <eric@hellman.net>
2020-12-08 14:20:39 -0500

    delint

--- BibrecPage.py
+++ BibrecPage.py
@@ -24,33 +24,33 @@ import BaseSearcher
 import Page
 
 
-class BibrecPage (Page.Page):
+class BibrecPage(Page.Page):
     """ Implements the bibrec page. """
 
-    def index (self, **dummy_kwargs):
+    def index(self, **dummy_kwargs):
         """ A bibrec page. """
 
-        os = BaseSearcher.OpenSearch ()
+        os = BaseSearcher.OpenSearch()
 
-        os.log_request ('bibrec')
+        os.log_request('bibrec')
 
-        dc = BaseSearcher.DC ()
+        dc = BaseSearcher.DC()
 
         # the bulk of the work is done here
-        dc.load_from_database (os.id)
+        dc.load_from_database(os.id)
         if not dc.files:
             # NOTE: Error message
-            raise cherrypy.HTTPError (404, _('No ebook by that number.'))
+            raise cherrypy.HTTPError(404, _('No ebook by that number.'))
 
         # add these fields so we won't have to test for their existence later
         dc.extra_info = None
         dc.url = None
 
-        dc.translate ()
-        dc.header = gg.cut_at_newline (dc.title)
-        os.title = dc.make_pretty_title ()
+        dc.translate()
+        dc.header = gg.cut_at_newline(dc.title)
+        os.title = dc.make_pretty_title()
         dc.extra_info = ''
-        dc.class_ = BaseSearcher.ClassAttr ()
+        dc.class_ = BaseSearcher.ClassAttr()
         dc.order = 10
         dc.icon = 'book'
         if 'Sound' in dc.categories:
@@ -59,54 +59,54 @@ class BibrecPage (Page.Page):
         os.twit = os.title
         os.qrcode_url = '/cache/epub/%d/pg%d.qrcode.png' % (os.id, os.id)
 
-        os.entries.append (dc)
+        os.entries.append(dc)
 
         s = cherrypy.session
-        last_visited = s.get ('last_visited', [])
-        last_visited.append (os.id)
+        last_visited = s.get('last_visited', [])
+        last_visited.append(os.id)
         s['last_visited'] = last_visited
 
         # can we find some meaningful breadcrumbs ?
         for a in dc.authors:
             if a.marcrel in ('aut', 'cre'):
-                book_cnt = BaseSearcher.sql_get (
+                book_cnt = BaseSearcher.sql_get(
                     "select count (*) from mn_books_authors where fk_authors = %(aid)s", aid = a.id)
                 if book_cnt > 1:
-                    os.breadcrumbs.append ((
-                        __('One by {author}', '{count} by {author}', book_cnt).format (
-                                count = book_cnt, author = dc.make_pretty_name (a.name)),
+                    os.breadcrumbs.append((
+                        __('One by {author}', '{count} by {author}', book_cnt).format(
+                                count = book_cnt, author = dc.make_pretty_name(a.name)),
                         _('Find more ebooks by the same author.'),
-                         os.url ('author', id = a.id)
+                         os.url('author', id = a.id)
                         ))
 
 
         if os.format == 'html':
-            cat = BaseSearcher.Cat ()
+            cat = BaseSearcher.Cat()
             cat.header = _('Similar Books')
             cat.title = _('Readers also downloaded…')
             cat.rel = 'related'
-            cat.url = os.url ('also', id = os.id)
+            cat.url = os.url('also', id = os.id)
             cat.class_ += 'navlink grayed noprint'
             cat.icon = 'suggestion'
             cat.order = 30
-            os.entries.append (cat)
+            os.entries.append(cat)
 
             for bookshelf in dc.bookshelves:
-                cat = BaseSearcher.Cat ()
-                cat.title = _('In {bookshelf}').format (bookshelf = bookshelf.bookshelf)
+                cat = BaseSearcher.Cat()
+                cat.title = _('In {bookshelf}').format(bookshelf = bookshelf.bookshelf)
                 cat.rel = 'related'
-                cat.url = os.url ('bookshelf', id = bookshelf.id)
+                cat.url = os.url('bookshelf', id = bookshelf.id)
                 cat.class_ += 'navlink grayed'
                 cat.icon = 'bookshelf'
                 cat.order = 33
-                os.entries.append (cat)
+                os.entries.append(cat)
 
         os.total_results = 1
 
         os.template = 'bibrec'
         os.page = 'bibrec'
         os.og_type = 'book'
-        os.finalize ()
-        rendered = self.format (os)
+        os.finalize()
+        rendered = self.format(os)
         dc.session.close()
         return rendered
